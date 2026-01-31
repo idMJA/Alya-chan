ed5436dc535e9373468214e509b339ade9abd534
iaMJ <kiyomi@mjba.my>
2026-01-31 14:59:41 +0700

    tired af

--- src/commands/fun/mod.rs
+++ src/commands/fun/mod.rs
@@ -1,11 +1,11 @@
 pub mod fake_tweet;
 pub mod hack;
 pub mod iq;
-pub mod ship;
+// pub mod ship;
 pub mod waifu;
 
 pub use fake_tweet::FakeTweetCommand;
 pub use hack::HackCommand;
 pub use iq::IqCommand;
-pub use ship::ShipCommand;
+// pub use ship::ShipCommand;
 pub use waifu::WaifuCommand;
